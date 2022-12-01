430d99867f3db4313d9c8c0a74e67cfcaa88bb2a
Joe Meyer <joseph.c.meyer@gmail.com>
2022-12-01 00:01:00 -0600

    player functionality and color output

--- Jeopardy/Jeopardy.ino
+++ Jeopardy/Jeopardy.ino
@@ -1,44 +1,65 @@
 #include "Button.h"
+#include "Player.h"
 #include <Adafruit_NeoPixel.h>
 
-const int button1pin = 15;
-const int button2pin = 16;
-const int button3pin = 17;
-const int light1pin = 12;
-const int light2pin = 13;
-const int light3pin = 14;
-const int buzzerPin = 9;
-const int clearButtonPin = 10;
-const int neopixelPin = 11;
-
+const int button1pin = 12;
+const int button2pin = 6;
+const int button3pin = 7;
+const int light1pin = 13;
+const int light2pin = 3;
+const int light3pin = 4;
+const int buzzerPin = 16;
+const int clearButtonPin = 2;
+const int neopixelPin = 14;
+int num_of_teams_in = 0;
+int playerIn = 0;
 
 #define NUMPIXELS 20 // Popular NeoPixel ring size
 
-Button Team1(button1pin, &Team1buzz, 30);
-Button ClearBtn(clearButtonPin, &Clear, 30); //pin, func ref, (optional) debounce
 Adafruit_NeoPixel pixels(NUMPIXELS, neopixelPin, NEO_GRB + NEO_KHZ800);
 
+int NUMBER_OF_PLAYERS = 3;
+int order[] = {9, 9, 9};
+Player players[] = {
+    Player(light1pin, button1pin, pixels.Color(0, 50, 0)), //TODO Store Player color 
+    Player(light2pin, button2pin, pixels.Color(0, 0, 50)),
+    Player(light3pin, button3pin, pixels.Color(50, 0, 0))};
 
-void setup() {
-  
+Button ClearBtn(clearButtonPin, &Clear, 30); // pin, func ref, (optional) debounce
+
+void setup()
+{
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 
+  pixels.setPixelColor(10, pixels.Color(0, 15, 0));
+
+  pixels.show(); // Send the updated pixel colors to the hardware.
 }
 
-void loop() 
+void loop()
 {
-  Team1.idle();
-};
- 
- void Clear()
+  for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
   {
-    pixels.clear(); // Set all pixel colors to 'off'    
-    pixels.show();   // Send the updated pixel colors to the hardware.
-  }
+    if (players[i].CheckPlayerIn())
+    {
+      order[playerIn] = i;
 
-  void Team1buzz()
-  {
-    
-    pixels.setPixelColor(1, pixels.Color(0, 255, 0));
-    pixels.show();   // Send the updated pixel colors to the hardware.
+      pixels.setPixelColor(playerIn, players[i].getColor()); // Set the color for the Player who buzzed in using the stored color
+      pixels.show(); // Send the updated pixel colors to the hardware.
+
+      playerIn++;
+    }
   }
+  ClearBtn.update();
+};
+
+void Clear(int state)
+{
+  playerIn = 0;
+  for (int i = 0; i < NUMBER_OF_PLAYERS; i++)
+  {
+    players[i].reset();
+  }
+  pixels.clear(); // Set all pixel colors to 'off'
+  pixels.show();  // Send the updated pixel colors to the hardware.
+}
---
+++ Jeopardy/Player.h
@@ -0,0 +1,76 @@
+// Library for creating jeopardy players.
+
+// ensure this library description is only included once
+#ifndef Player_h
+#define Player_h
+
+#include "Arduino.h"
+
+class Player
+{
+private:
+    int button_pin;
+    int light_pin;
+    int number;
+    int color;
+    // unsigned long currentMillis;
+    // unsigned long lastMillis;
+
+public:
+    bool isIn = false;
+    Player( int, int, int);
+    bool CheckPlayerIn();
+    void lightOn();
+    void lightOff();
+    int getColor();
+    void reset();
+};
+
+// player number, light pin, button pin.
+Player::Player(int _l, int _b, int _c)
+{
+    button_pin = _b;
+    light_pin = _l;
+    color = _c;
+    pinMode(button_pin, INPUT_PULLUP);
+    pinMode(light_pin, OUTPUT);
+    lightOn();
+}
+
+void Player::lightOn()
+{
+    digitalWrite(light_pin, HIGH);
+}
+
+void Player::lightOff()
+{
+    digitalWrite(light_pin, LOW);
+}
+
+int Player::getColor()
+{
+  return(color);
+}
+
+void Player::reset()
+{
+  isIn = false;
+  lightOn();
+}
+
+bool Player::CheckPlayerIn()
+{
+    if (!(isIn)) // If not in
+    {
+        if (!digitalRead(button_pin)) // button is pushed
+        {
+            isIn = true;
+            lightOff();
+            return(isIn);
+        }
+        return(0);
+    }
+    return(0);
+};
+
+#endif
